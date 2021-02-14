06b29e7db783cc91c105d3c01da898b63b104b47
dustin daugherty <jsu0583n@stu.jsu.edu>
2021-02-13 21:26:17 -0600

    filled in csvToJson, works test not passsed

--- src/edu/jsu/mcis/Converter.java
+++ src/edu/jsu/mcis/Converter.java
@@ -68,7 +68,29 @@ public class Converter {
             Iterator<String[]> iterator = full.iterator();
             
             // INSERT YOUR CODE HERE
+            JSONObject json = new JSONObject();
+            JSONArray col = new JSONArray();
+            JSONArray row = new JSONArray();
+            JSONArray data = new JSONArray();
+            JSONArray holder = new JSONArray();
+            String[] record = iterator.next();
             
+            for(int i = 0; i < record.length; i++){
+                col.add(record[i]);
+            }
+            while(iterator.hasNext()){
+                record = iterator.next();
+                row.add(record[0]);
+                for(int i = 1; i < record.length; i++){
+                    int stringHolder = Integer.parseInt(record[i]);
+                    holder.add(stringHolder);
+                }
+                data.add(holder);
+            }
+            json.put("rowHeaders", row);
+            json.put("colHeaders", col);
+            json.put("data", data);
+            results = JSONValue.toJSONString(json);
         }        
         catch(Exception e) { return e.toString(); }
         
